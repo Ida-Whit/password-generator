895a287bab587e538985ea09534d019f445dd3ff
Ida Whitcomb <idawhitcomb@gmail.com>
2023-10-22 16:43:26 -0400

    Added README and edited JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,11 @@ const lower = "abcdefghijklmnopqrstuvwxyz"
 
 let select = []
 
-const generateBtn = document.querySelector("#generate");   // Assignment Code  // linked to the button on webpage
+const generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword);  // Add event listener to generate button  // makes it so that when you click the button, it will execute the writePassword function.
+generateBtn.addEventListener("click", writePassword); 
 
-function writePassword() {
+function writePassword(){
   const userAmount  = prompt("Please select a password length between 8 and 128 characters.", "8-128")
   const userChar = confirm("Would you like to include special characters?")
   const userUpper = confirm("Would you like to include uppercase letters?")
@@ -30,15 +30,18 @@ function writePassword() {
   }
 
   function generatePassword(){
-    for (let i=0; i<userAmount.length; i++) {
-      Math.floor(Math.random() * select.length)
-    };
-  }
+    let password = ""
+    for (let i=0; i<userAmount; i++) {
+      let randomSelect = Math.floor(Math.random() * select.length)
+      let randomCharacter = Math.floor(Math.random() * select[randomSelect].length)
+      password += select[randomSelect][randomCharacter]
+      }
+      return password
+  };
+  
 
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
-
-  const password = generatePassword()
-  const passwordText = document.querySelector("#password");  // passwordText linked to text area on webpage
-
-  passwordText.value = password; // links the passwordText (linked to the text area on the webpage) with the password that will be generated once the code is entered.
+  passwordText.value = password;
 }
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# password-generator
-Random Password Generator
+## Random Password Generator
+
+## Description
+This is a random password generator. When you click on the Generate Password button the program prompts you with 5 questions; Choose the length you want for your password, would you like to include uppercase letters, would you like to include lowercase letters, would you like to include special characters, and would you like to include numbers. Once the user has successfully answered all these questions the program creates an array containing only those characters that the user has said yes to. Once the function gets set in motion it will iterate through this array and randomly pick out characters for your password. Once this function has completed it will display your password on the screen.
+
+## Visuals
+![Alt text](<Develop/Password Generator.png>)
+
+## Usage
+This program is meant to be used to generate a secure, random password for anything you might need.
+
+## Roadmap
+N/A
+
+## Contributing
+N/A
+
+## Authors and acknowledgment
+HTML and CSS sheets were written by UNH Coding Bootcamp as a starter for this project. .addEventListener and .querySelector components in the JavaScript file were also written by UNH Coding Bootcamp. Everything else on the JavaScript page was written by myself, Ida Whitcomb, with guidance from the teacher of Coding Bootcamp, Ladislav Jerabek.
+
+## License
+N/A
+
+## Project status
+This project is complete, no further edits planned.
